198a81c2d9a3cf18ff92c33bbc9788768da1fa41
Martin Galvan <md.galvan@uniandes.edu.co>
2021-03-19 12:35:18 -0500

    Corrections

--- testFiles/downloadFiles.sh
+++ testFiles/downloadFiles.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -O 100MBFile.bin https://www.dropbox.com/s/gqo5v7kblsop98s/100MBFile.bin
-wget -O 200MBFile.bin https://www.dropbox.com/s/tbs0p9zpmyeuihd/250MBFile.bin
+wget -O 250MBFile.bin https://www.dropbox.com/s/tbs0p9zpmyeuihd/250MBFile.bin
